dca53ce02122c5d13fd2f13ddb74b5c45a2afbb7
amanullayev2210 <karory577@gmail.com>
2025-01-06 15:51:56 +0500

    Add new functions and fix bugs

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.querySelector(".js-button").addEventListener("click", (evt) => {
       .classList.toggle("open");
   });
 
-  document.querySelectorAll('.hero__link').forEach(link => {
+  document.querySelectorAll('.js__link').forEach(link => {
     link.addEventListener('click', function (e) {
       e.preventDefault(); 
 
@@ -19,6 +19,43 @@ document.querySelector(".js-button").addEventListener("click", (evt) => {
       const targetPosition = targetSection.offsetTop;
       const distance = targetPosition - startPosition;
       const duration = 1800; 
+      let startTime = null;
+
+      function scrollAnimation(currentTime) {
+        if (!startTime) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+
+        if (timeElapsed < duration) {
+          requestAnimationFrame(scrollAnimation);
+        }
+      }
+
+      function ease(t, b, c, d) {
+        const easeInOutQuad = t => {
+          if ((t /= d / 2) < 1) return (c / 2) * t * t + b;
+          return (-c / 2) * (--t * (t - 2) - 1) + b;
+        };
+        return easeInOutQuad(t);
+      }
+
+      requestAnimationFrame(scrollAnimation);
+    });
+  });
+
+  document.querySelectorAll('.main-js__link').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault(); 
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      // Custom scroll animatsiya
+      const startPosition = window.pageYOffset;
+      const targetPosition = targetSection.offsetTop;
+      const distance = targetPosition - startPosition;
+      const duration = 1100; 
       let startTime = null;
 
       function scrollAnimation(currentTime) {
